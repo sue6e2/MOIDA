e8242a82c25f5a657b5954126eaf5f4374b7d2c7
주연 <lpjy1107@gmail.com>
2021-11-04 21:01:49 +0900

    MOIDA-8 react-router-dom 설정

---
+++ src/Page/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './LandingPage.css';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div className="LandingPage">
+                <p>랜딩페이지를 만드는 곳입니다</p>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
