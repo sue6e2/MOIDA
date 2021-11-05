16c6b686bd0c4842f094880e08e4cc28fbc8db4e
주연 <lpjy1107@gmail.com>
2021-11-05 16:22:05 +0900

    MOIDA-5 랜딩페이지 버튼 구현

--- src/Page/LandingPage/LandingPage.jsx
+++ src/Page/LandingPage/LandingPage.jsx
@@ -6,6 +6,7 @@ class LandingPage extends Component {
         return (
             <div className="LandingPage">
                 <p>랜딩페이지를 만드는 곳입니다</p>
+                <button onClick={() => { window.location.href = "/SignIn" }}>로그인</button>
             </div>
         );
     }
