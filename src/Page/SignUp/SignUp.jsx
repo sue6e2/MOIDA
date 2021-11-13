78266c48db1d457fbdb695b4cebb4d1153d6eaeb
주연 <lpjy1107@gmail.com>
2021-11-14 04:22:11 +0900

    MOIDA-4 CongratulationPage UI 구현 완료

--- src/Page/SignUp/SignUp.jsx
+++ src/Page/SignUp/SignUp.jsx
@@ -3,40 +3,40 @@ import './SignUp.css'
 import axios from "axios";
 
 class SignUp extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            id : "",
-            password : "",
-            passwordCheck : "",
-            name : ""
+            id: "",
+            password: "",
+            passwordCheck: "",
+            name: ""
         };
     }
 
     onChangeID = (e) => {
         this.setState({
-            id : e.target.value
+            id: e.target.value
         })
         console.log(this.state.id);
     }
 
     onChangePassword = (e) => {
         this.setState({
-            password : e.target.value
+            password: e.target.value
         })
         console.log(this.state.password);
     }
 
     onChangePasswordCheck = (e) => {
         this.setState({
-            passwordCheck : e.target.value
+            passwordCheck: e.target.value
         })
         console.log(this.state.passwordCheck);
     }
 
     onChangeName = (e) => {
         this.setState({
-            name : e.target.value
+            name: e.target.value
         })
         console.log(this.state.name);
     }
@@ -52,57 +52,57 @@ class SignUp extends Component {
 
     render() {
         return (
-            <div className = "container_">
-            <div className = "SignUp">
-                <head>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-                </head>
-                <form className = "frmSignUp" name = "signup" method = "post" onSubmit = {this.callApi()} >
-                    <div className = "content">
-                    <h1 className = "text-center my-4">MOIDA</h1>
-                    <div className = "text-start">아이디</div>
-                    <div class="input-group mt-1 mb-3">
-                        <input
-                            type="text"
-                            class="form-control"
-                            required
-                            onChange = {this.onChangeID} />
-                        <button class="bt btn-main" type="button" id="button-addon2">중복확인</button>
-                    </div>
-                    <div className = "text-start">비밀번호</div>
-                    <div class="input-group mt-1 mb-3">
-                        <input
-                            type="password"
-                            class="form-control"
-                            required
-                            onChange = {this.onChangePassword} />
-                    </div>
-                    <div className = "text-start">비밀번호 확인</div>
-                    <div class="input-group mt-1 mb-3">
-                        <input
-                            type="password"
-                            class="form-control"
-                            required
-                            onChange = {this.onChangePasswordCheck} />
-                    </div>
-                        <span className = "d-none">비밀번호가 일치합니다.</span>
-                        <span className = "d-none">비밀번호가 일치하지 않습니다.</span>
-                    <div className = "text-start">이름</div>
-                    <div class="input-group mt-1 mb-3">
-                        <input
-                            type="text"
-                            class="form-control"
-                            required
-                            onChange = {this.onChangeName} />
-                    </div>
-                    <div className = "d-grid gap-2">
-                        <button className = "bt btn-main my-3" type = "submit">가입하기</button>
-                    </div>
-                    <p>이미 계정이 있으신가요? <button type = "button" className = "btn-a" onClick = "">로그인</button></p>
-                    </div>
-                </form>
-            </div>
+            <div className="container_">
+                <div className="SignUp">
+                    <head>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                    </head>
+                    <form className="frmSignUp" name="signup" method="post" onSubmit={this.callApi()} >
+                        <div className="content">
+                            <h1 className="text-center my-4">MOIDA</h1>
+                            <div className="text-start">아이디</div>
+                            <div class="input-group mt-1 mb-3">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    required
+                                    onChange={this.onChangeID} />
+                                <button class="bt btn-main" type="button" id="button-addon2">중복확인</button>
+                            </div>
+                            <div className="text-start">비밀번호</div>
+                            <div class="input-group mt-1 mb-3">
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    required
+                                    onChange={this.onChangePassword} />
+                            </div>
+                            <div className="text-start">비밀번호 확인</div>
+                            <div class="input-group mt-1 mb-3">
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    required
+                                    onChange={this.onChangePasswordCheck} />
+                            </div>
+                            <span className="d-none">비밀번호가 일치합니다.</span>
+                            <span className="d-none">비밀번호가 일치하지 않습니다.</span>
+                            <div className="text-start">이름</div>
+                            <div class="input-group mt-1 mb-3">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    required
+                                    onChange={this.onChangeName} />
+                            </div>
+                            <div className="d-grid gap-2">
+                                <button className="bt btn-main my-3" type="submit">가입하기</button>
+                            </div>
+                            <p>이미 계정이 있으신가요? <button type="button" className="btn-a" onClick={() => { window.location.href = "/SignIn" }}>로그인</button></p>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
