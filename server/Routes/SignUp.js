ed9086f72e01c589389eda9c580f3ac3006eb023
주연 <lpjy1107@gmail.com>
2021-11-14 16:12:43 +0900

    MOIDA-27, MOIDA-65, MOIDA-26 완료 SignUp페이지 회원가입 및 중복확인 기능 완료했습니다

--- server/Routes/SignUp.js
+++ server/Routes/SignUp.js
@@ -15,20 +15,34 @@ const connection = mysql.createConnection({
 connection.connect();
 
 router.post('/', function (req, res) {
-
   let account_id = req.body.params.account_id;
   let account_pwd = req.body.params.account_pwd;
   let account_name = req.body.params.account_name;
+
   connection.query('insert into account (account_id,account_pwd,account_name) values ("' + account_id + '","' + account_pwd + '","' + account_name + '")', function (err, rows) {
     if (err) {
       console.log(err);
       return res.send({ code: 101, errorMessage: err });
     }
     else {
+      console.log(rows)
       res.send({ code: 0, data: rows });
     }
   })
 
+})
+
+router.get('/idDoubleCheck', function (req, res) {
+  let account_id = req.query.account_id;
+  let sql = 'select id from account where account_id=?';
+  connection.query(sql, account_id, function (err, rows, fields) {
+    if (rows[0] === undefined) {
+      res.send({ code: 0 });
+    }
+    else {
+      res.send({ code: 101 });
+    }
+  });
 })
 
 module.exports = router;
