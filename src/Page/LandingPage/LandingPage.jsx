ef7bdd3087314c87d628e38f92bfb1f74f65e02d
주연 <lpjy1107@gmail.com>
2021-11-28 23:01:29 +0900

    랜딩페이지 구조잡기

--- src/Page/LandingPage/LandingPage.jsx
+++ src/Page/LandingPage/LandingPage.jsx
@@ -1,12 +1,26 @@
 import { Component } from 'react';
 import './LandingPage.css';
+import logo from '../../res/img/logo.png';
+import GifPlayer from "react-gif-player";
+import Pengsu from "../../res/img/instagram.gif";
 
 class LandingPage extends Component {
     render() {
         return (
             <div className="LandingPage">
-                <p>랜딩페이지를 만드는 곳입니다</p>
-                <button onClick={() => { window.location.href = "/SignIn" }}>로그인</button>
+                <div className="LandingPageTop">
+                    <img src={logo} alt="로고" />
+                    <p className="SignInBt" onClick={() => { window.location.href = "/SignIn" }}>로그인</p>
+                    <button className="SignUpBt" onClick={() => { window.location.href = "/SignUp" }}>시작하기</button>
+                </div>
+
+                <div className="VideoOne">
+                    <GifPlayer autoplay={true} gif={Pengsu} ></GifPlayer>
+                </div>
+                <button className="StartBt">시작하기</button>
+                <div className="VideoTwo">
+                    <GifPlayer autoplay={true} gif={Pengsu} ></GifPlayer>
+                </div>
             </div>
         );
     }
